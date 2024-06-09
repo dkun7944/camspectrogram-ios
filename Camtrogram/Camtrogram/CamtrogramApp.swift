e79e1496da3b798a4a3e6eab36bad85b66a9af45
Daniel Kuntz <dkuntz0@gmail.com>
2024-06-08 20:47:17 -0700

    Initial commit

---
+++ Camtrogram/Camtrogram/CamtrogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamtrogramApp.swift
+//  Camtrogram
+//
+//  Created by Daniel Kuntz on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CamtrogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
